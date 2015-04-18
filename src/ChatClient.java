64293f3225b52db894f1d4753a7e9cc8a6b9de79
ACHinrichs <adrianhinrichs@outlook.de>
2015-04-18 10:29:21 +0200

    +processing of messages in client completed

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -6,27 +6,63 @@ import java.io.*;
  * @author A.C.Hinrichs
  * @version 2015-04-16
  */
-public class ChatClient extends JFrame implements ActionListener
+public class ChatClient implements ActionListener
 {
     private Connection connection;
+    private Receiver receiver;
+    
+    public static boolean output = true; 
+    
     public ChatClient(String pIPAdresse, int pPortNr)
     {
         //super(pIPAdresse,pPortNr);
+        super();
+        
         connection = new Connection(pIPAdresse,pPortNr);
+        receiver = new Receiver(this, connection);
+        receiver.start();
     }
     
     public void process(String pMessage){
-        String[] msgParts = pMessage.split(" ");
-        if(msgParts[0].equals("MSG")){
-            //msgParts[2] ist die nachricht, asugeben!
-            connection.send("GOTMSG "+msgParts[1]);
-        }else if(msgParts[0].equals("BRD")){
-            //msgParts[2] ist die nachricht, asugeben!
-            connection.send("GOTBRD "+msgParts[1]);
+        System.out.println("test");
+        System.out.println(pMessage);
+        if(pMessage.matches("\\bMSG\\b\\s.+\\bFROM\\b\\s.+\\bWITH\\b\\s.+")){
+            String ohneMsg = pMessage.substring(2,pMessage.length()-1);
+            String msg = pMessage.split(" FROM ")[0];
+            String src = (pMessage.split(" FROM ")[1]).split(" WITH ")[0];
+            String id = pMessage.split("WITH")[1];
+            
+            System.out.println("("+id+") "+src+": "+msg);
+            
+            connection.send("GOTMSG "+id);
+        }else if(pMessage.matches("\\bBRD\\b\\s.+\\bFROM\\b\\s.+\\bWITH\\b\\s.+")){
+            String ohneMsg = pMessage.substring(2,pMessage.length()-1);
+            String msg = pMessage.split(" FROM ")[0];
+            String src = (pMessage.split(" FROM ")[1]).split(" WITH ")[0];
+            String id = pMessage.split("WITH")[1];
+            
+            System.out.println("("+id+") "+src+": "+msg);
+            
+            connection.send("GOTBRD "+id);
+        }else if(pMessage.matches("\\bLST\\b\\s.+")){
+            String ohneLst = pMessage.substring(2,pMessage.length()-1);
+            String[] nutzerliste = pMessage.split(" ");
+            System.out.println("Liste der nutzer: ");
+            for(String i:nutzerliste){
+                System.out.println("     "+i);
+            }
+        }else if(output && pMessage.matches("\\bOK\\b")){
+            System.out.println("Server bestätigt "+pMessage);
+        }else if(output && pMessage.matches("\\bERR\\b")){
+            System.out.println("Server fehler "+pMessage);
         }
     }
     
     public void actionPerformed(ActionEvent e){
+        
+    }
     
+    public void send(String msg){
+        connection.send(msg);
     }
 }
