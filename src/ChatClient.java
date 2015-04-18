5fc29d3ca910b12d3f76897804b07af2891a44cf
ACHinrichs <adrianhinrichs@outlook.de>
2015-04-18 11:28:01 +0200

    +added ClientGui

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,15 +1,13 @@
-import javax.swing.*;
-import java.awt.event.*;
-import java.io.*;
 /**
  * GOTMSG-CHat-CLient
  * @author A.C.Hinrichs
- * @version 2015-04-16
+ * @version 2015-04-18
  */
-public class ChatClient implements ActionListener
+public class ChatClient
 {
     private Connection connection;
     private Receiver receiver;
+    private ClientGUI gui;
     
     public static boolean output = true; 
     
@@ -21,6 +19,8 @@ public class ChatClient implements ActionListener
         connection = new Connection(pIPAdresse,pPortNr);
         receiver = new Receiver(this, connection);
         receiver.start();
+        
+        gui = new ClientGUI(this);
     }
     
     public void process(String pMessage){
@@ -32,7 +32,7 @@ public class ChatClient implements ActionListener
             String src = (pMessage.split(" FROM ")[1]).split(" WITH ")[0];
             String id = pMessage.split("WITH")[1];
             
-            System.out.println("("+id+") "+src+": "+msg);
+            output("("+id+") "+src+": "+msg);
             
             connection.send("GOTMSG "+id);
         }else if(pMessage.matches("\\bBRD\\b\\s.+\\bFROM\\b\\s.+\\bWITH\\b\\s.+")){
@@ -41,28 +41,28 @@ public class ChatClient implements ActionListener
             String src = (pMessage.split(" FROM ")[1]).split(" WITH ")[0];
             String id = pMessage.split("WITH")[1];
             
-            System.out.println("("+id+") "+src+": "+msg);
+            output("("+id+") "+src+": "+msg);
             
             connection.send("GOTBRD "+id);
         }else if(pMessage.matches("\\bLST\\b\\s.+")){
             String ohneLst = pMessage.substring(2,pMessage.length()-1);
-            String[] nutzerliste = pMessage.split(" ");
-            System.out.println("Liste der nutzer: ");
+            String[] nutzerliste = pMessage.split(";");
+            output("Liste der nutzer: ");
             for(String i:nutzerliste){
-                System.out.println("     "+i);
+                output("     "+i);
             }
-        }else if(output && pMessage.matches("\\bOK\\b")){
-            System.out.println("Server bestätigt "+pMessage);
-        }else if(output && pMessage.matches("\\bERR\\b")){
-            System.out.println("Server fehler "+pMessage);
+        }else if(pMessage.matches("\\bOK\\b")){
+            output("Server bestätigt "+pMessage);
+        }else if(pMessage.matches("\\bERR\\b")){
+            output("Server fehler "+pMessage);
         }
-    }
-    
-    public void actionPerformed(ActionEvent e){
-        
     }
     
     public void send(String msg){
         connection.send(msg);
+    }
+    
+    private void output(String text){
+        gui.write(text);
     }
 }
